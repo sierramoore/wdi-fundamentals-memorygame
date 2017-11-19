c3ef9e5760f823a26adc3c0de86a034ff7151698
Sierra Moore <sierra.v.moore@gmail.com>
2017-11-19 13:39:30 -0600

    added more game logic

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-var cardTwo = cards[2] ;
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === [2] && cardsInPlay.length === [0]){
-    alert("You found a match!");
+var checkForMatch = function (){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again.");
+    }
 }
-else
-    alert("Sorry, try again.");
+var flipCard = function(cardId){
+    checkForMatch();
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    }
+    else
+        alert("Sorry, try again.");
+    }
 
-/*
-var cardThree = ;
-var cardFour = ;
-console.log("User flipped " + cardThree);
-*/
+flipCard(0);
+flipCard(2);
+
 
