d9421bf589469673b97aeca28b4424573cbe3a5f
Sierra Moore <sierra.v.moore@gmail.com>
2017-11-19 01:52:39 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cardOne = cards[0];
+var cardTwo = cards[2] ;
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === [2] && cardsInPlay.length === [0]){
+    alert("You found a match!");
+}
+else
+    alert("Sorry, try again.");
+
+/*
+var cardThree = ;
+var cardFour = ;
 console.log("User flipped " + cardThree);
+*/
+
