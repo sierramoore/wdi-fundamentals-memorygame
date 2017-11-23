469bb572c926445efe6fa38043860c6b2d7ddfac
Sierra Moore <sierra.v.moore@gmail.com>
2017-11-23 01:47:37 -0600

    created game logic again

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit:"hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 var checkForMatch = function (){
     if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -6,19 +27,35 @@ var checkForMatch = function (){
     } else {
         console.log("Sorry, try again.");
     }
-}
-var flipCard = function(cardId){
+
+};
+var flipCard = function () {
+    var cardId = this.getAttribute('data-id');
     checkForMatch();
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
-    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
-        alert("You found a match!");
+    cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
+    if (cardsInPlay.length === 2) {     // can't do these to if statements in one using &&
+        if (cardsInPlay[0] === cardsInPlay[1]) { // bkus when the length != 2 the else statement runs
+            alert("You found a match!");
+        }
+        else {
+            alert("Sorry, try again.");
+        }
     }
-    else
-        alert("Sorry, try again.");
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].cardSuit);
+};
+
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
     }
+};
 
-flipCard(0);
-flipCard(2);
-
+createBoard();
 
